08ea9f08f2d5ff1cef412a3927e1d6bc764f6fc3
renata-rodriguess <urenata78@gmail.com>
2026-04-16 09:36:01 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cabecalho from './Cabecalho';
+import Projetos from'./Projetos';
+funcition App(){
+return(
+<div className="App">
+<Cabecalho/>
+<Projetos/>
+</div>
+);
+}
+export default App;
